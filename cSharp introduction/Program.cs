23536b0bb39ac6dce152860170d5c7452f8a717f
MattM424 <ginger424901@gmail.com>
2018-01-23 13:51:35 -0500

    This is the first commit

---
+++ cSharp introduction/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharp_introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variable
+            double Input;
+
+            //input
+            Console.WriteLine("Please enter a number! Like now!");
+            //Input = Convert.ToInt32(Console.ReadLine());
+            Input = double.Parse(Console.ReadLine());
+
+            //processing
+
+            Input = Math.Sqrt(Input);
+
+            //output
+
+            //Console.WriteLine("Output is equal to: "+Input);
+            Console.WriteLine($"Output is equal to {Input}");
+
+
+        }
+    }
+}
